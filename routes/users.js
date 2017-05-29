5d7fd3428bfb765bd4cce6cc4324a6dfb2592f0e
Laci <elwindly@gmail.com>
2017-05-29 22:16:33 +0200

    Add user auth controller

--- routes/users.js
+++ routes/users.js
@@ -6,70 +6,13 @@ const {authenticate} = require('./../middleware/authenticate');
 
 const {User} = require('./..//models/members');
 
-/* GET users listing. */
-router.get('/',function(req, res, next) {
-  res.send('respond with a resource');
-});
+const UserAuthController = require('./../controllers/userAuthController');
 
-router.post('/', (req,res)=> {
-    var body = _.pick(req.body,['email','name','password']);
-
-    User.findOne({email:body.email}).then((user)=>{
-    if(user){
-        res.status(409).send({message:'User already exist'});
-    }else{
-        User.findOne({name:body.name}).then((user)=>{
-            if(user){
-                res.status(409).send({message:'User already exist'});     
-            }else{
-               var user = new User(body);
-
-                user.save().then(()=>{
-                    req.session.name = body.name;
-                    req.session._id = user._id;
-                   // console.log("user session id",req.session._id)
-                    return user.generateAuthToken();
-                }).then((token)=>{           
-                    //res.header('x-auth', token).send(user);
-                    req.session.xAuth = token;
-                    res.status(200).send();
-                }).catch((e)=>{
-                    res.status(400).send()
-                });
-            }       
-        });
-    }
-    });
-});
+const userAuthController = new UserAuthController();
 
 
-router.post('/login',(req,res)=>{
-    var body = _.pick(req.body,['emailLog','pwdLog']);
-
-    User.findByCredentials(body.emailLog,body.pwdLog).then((user)=>{
-        return user.generateAuthToken().then((token)=>{
-            req.session.xAuth = token;
-            req.session.name = user.name;
-            req.session._id = user._id;
-            res.redirect('/userLogged');
-        });
-    }).catch((e)=>{
-        res.status(409).send({message: "Invalid credinials!"});
-    });
-});
-
-router.delete('/me/token',authenticate,(req,res)=>{
-    req.user.removeToken(req.session.xAuth).then(()=>{
-        req.session.xAuth = null;
-        req.session.name = null;
-        req.session._id = null;
-        req.session.destroy(function(err) {
-           res.status(200).send();
-        })
-        
-    },()=>{
-        res.status(400).send();
-    });
-});
+router.post('/', userAuthController.signUp);
+router.post('/login', userAuthController.logIn);
+router.delete('/me/token',authenticate, userAuthController.logOut);
 
 module.exports = router;
